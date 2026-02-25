9b7d441cc9590d000a4ff08756abaf05e588d9d3
Lucas Mafra <lucasmafra.med@gmail.com>
2026-02-25 12:35:53 -0300

    Adicionando Classe ContaPoupanca

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 import conta_bancaria.util.Cores;
 import conta_bancaria.model.Conta;
-import conta_bancaria.model.ContaCorrente;
 import conta_bancaria.model.ContaPoupanca;
 
 
@@ -25,14 +24,6 @@ public class Menu {
 
         c1.depositar(5000.0f);
 		c1.visualizar();
-		
-		// Teste da Classe Conta Corrente
-		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "José da Silva", 0.0f, 1000.0f);
-		cc1.visualizar();
-		cc1.sacar(12000.0f);
-		cc1.visualizar();
-		cc1.depositar(5000.0f);
-		cc1.visualizar();
 				
 		// Teste da Classe Conta Poupança
 		ContaPoupanca cp1 = new ContaPoupanca(3, 123, 2, "Maria dos Santos", 100000.0f, 15);
